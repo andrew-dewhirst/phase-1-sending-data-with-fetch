28baec04925d547f4182f23345e67bc139b6ed93
Andrew Dewhirst <andrew.dewhirst8@gmail.com>
2021-09-21 22:42:26 -0400

    commit tree 4322343b2604702676249ef96d59a877c50bc76d

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your code here
+function submitData(userName,userEmail) {
+  return fetch('http://localhost:3000/users', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+    body: JSON.stringify({
+      name: `${userName}`,
+      email: `${userEmail}`,
+    }),
+  })
+  .then(response => response.json())
+  .then(user => {
+    const identify = document.createElement('p');
+    identify.textContent = user.id;
+    document.body.appendChild(identify);
+  })
+  .catch(error => {
+    const errorMessage = document.createElement('p');
+    errorMessage.textContent = error.message;
+    document.body.appendChild(errorMessage);
+  })
+}
